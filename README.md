35145b898358366115374a36fdc697d4dd70430c
Simranjot singh <sing1239@algonquinlive.com>
2020-01-13 15:51:06 -0500

    participation 2

---
+++ README.md
@@ -0,0 +1,2 @@
+# Git JavaScript
+## Simranjot SIngh
---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+let name = 'Simranjot'
+console.log(`Hello, my name is ${name},`)
+
+let number = 5
+
+console.log(`The number ${number} multiply by 2 is ${number * 2},`)
+console.log(`The number multiplied by 5 is ${product * 5},`)
+console.log(`The result divided by 5 is ${(product * 5) / number}`)
+let finalValue = (product * 5) / number
+console.log(`The result minus 7 is ${finalValue * 7},`)
+
